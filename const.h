aa401922ba69b5d93d6e83417f3e9a23f10993c4
Oksana Skripkina <leedoor38@gmail.com>
2022-04-22 20:50:57 +0300

    const

---
+++ const.h
@@ -0,0 +1,2 @@
+float const pi = 3.14;
+float const e = 2.71;
---
+++ Проект Git.cpp
@@ -0,0 +1,13 @@
+﻿// Проект Git.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "const.h"
+using namespace std;
+int main()
+{
+	cout <<"pi = " << pi << endl;
+	cout <<"e = " << e << endl;
+}
+
+
